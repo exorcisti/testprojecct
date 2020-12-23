de35a1fda4316f0ac79f27ac59af46a03c5f0ae7
Aleksei <expresident14@gmail.com>
2020-12-23 17:14:27 +0300

    тест на цену и имя

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,8 +5,8 @@ from .locators import MainPageLocators
 
 class MainPage(BasePage): #наследник класса BasePage
     def go_to_login_page(self):
-       login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-       login_link.click()
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
 
     def should_be_login_link(self):
         #self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
--- test_main_page.py
+++ test_main_page.py
@@ -4,12 +4,17 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    
+    #Главнаая страница
     page = MainPage(browser, link, timeout=10)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # кликаем на кнопку — переходим на страницу логина
 
+    #Страница авторизации
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+  
+
 
 
 
